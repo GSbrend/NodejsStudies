fba7c4047486f8a8d193d695b0ee91a58974cef7
GSbrend <brendon.gs.dev@gmail.com>
2025-06-06 21:41:14 -0300

    modified:   2-how-node-works/starter/event-loop.js

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
 
+console.log('I/O finished');
 setTimeout(() => console.log('Time 1 is out!'), 0);
+
+fs.readFile('test-file.txt',() => {
+console.log('I/O finished');
+setTimeout(() => console.log('Time 2 is out!'), 0);
+setTimeout(() => console.log('Time 3 is out!'), 2000);
 setImmediate(() => console.log('immediate 1 finished'));
+});
 
-fs.readFile('text-file.txt',()=> {
-    console.log('I/O finished')
-})
-
-console.log('hello, top level code here!')
+console.log('hello, top level code here!');
