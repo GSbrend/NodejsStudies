5c67c08167e01e903cb0e04fd186eafbcd276694
GSbrend <brendon.gs.dev@gmail.com>
2025-06-08 15:27:32 -0300

    cahnged

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
 const start = Date.now();
-//
+//=========== HOW TO CHANGE THE THREADPOOLSIZE
+process.env.UV_THREADPOOL_SIZE = 2;
 console.log('I/O finished');
 //
 setTimeout(() => console.log('Time 1 is out!'), 0);
@@ -12,11 +13,21 @@ setTimeout(() => console.log('Time 2 is out!'), 0);
 setTimeout(() => console.log('Time 3 is out!'), 2000);
 setImmediate(() => console.log('immediate 1 finished'));
 
+
 process.nextTick(() => console.log('wtf'));
 
 crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>{
-    console.log(Date.now - start, 'password encrypted');
-})
+    console.log(Date.now() - start, 'password encrypted');
+});
+crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>{
+    console.log(Date.now() - start, 'password encrypted');
+});
+crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>{
+    console.log(Date.now() - start, 'password encrypted');
+});
+crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>{
+    console.log(Date.now() - start, 'password encrypted');
+});
 });
 
 console.log('hello, top level code here!');
