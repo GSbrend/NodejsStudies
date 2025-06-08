a9167c863ddae74a11c1dc681febd9cd59b95e84
GSbrend <brendon.gs.dev@gmail.com>
2025-06-08 15:17:19 -0300

    changes

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
-
+const crypto = require('crypto');
+const start = Date.now();
+//
 console.log('I/O finished');
+//
 setTimeout(() => console.log('Time 1 is out!'), 0);
 
 fs.readFile('test-file.txt',() => {
@@ -8,6 +11,12 @@ console.log('I/O finished');
 setTimeout(() => console.log('Time 2 is out!'), 0);
 setTimeout(() => console.log('Time 3 is out!'), 2000);
 setImmediate(() => console.log('immediate 1 finished'));
+
+process.nextTick(() => console.log('wtf'));
+
+crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>{
+    console.log(Date.now - start, 'password encrypted');
+})
 });
 
 console.log('hello, top level code here!');
