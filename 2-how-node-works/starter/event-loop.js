f5f8823ef7b9585e4e0737f04b3237f8be758891
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-09 12:45:32 +0000

    eventloop 4/4 assync

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -15,9 +15,9 @@ fs.readFile('test-file.txt',() => {
 
     process.nextTick(() => console.log('wtf'));
     
-    crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
-    console.log(Date.now() - start, 'password encrypted');
-    
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>{
+        console.log(Date.now() - start, 'password encrypted');
+    });
     crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>{
         console.log(Date.now() - start, 'password encrypted');
     });
