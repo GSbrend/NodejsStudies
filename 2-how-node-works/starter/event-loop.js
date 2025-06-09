6118a5242a32608f29a9c1975cab27e4973b372c
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-09 12:32:57 +0000

    .

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -15,7 +15,9 @@ fs.readFile('test-file.txt',() => {
 
     process.nextTick(() => console.log('wtf'));
     
-    crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+    crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512', () =>{
+        console.log(Date.now() - start, 'password encrypted');
+    });
     crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512', () =>{
         console.log(Date.now() - start, 'password encrypted');
     });
