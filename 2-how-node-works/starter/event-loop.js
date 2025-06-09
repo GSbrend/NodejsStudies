251a2059170e2292485ccfe67ecbfbf436bd20b2
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-09 12:31:54 +0000

    .

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -8,25 +8,25 @@ console.log('I/O finished');
 setTimeout(() => console.log('Time 1 is out!'), 0);
 
 fs.readFile('test-file.txt',() => {
-console.log('I/O finished');
-setTimeout(() => console.log('Time 2 is out!'), 0);
-setTimeout(() => console.log('Time 3 is out!'), 2000);
-setImmediate(() => console.log('immediate 1 finished'));
+    console.log('I/O finished');
+    setTimeout(() => console.log('Time 2 is out!'), 0);
+    setTimeout(() => console.log('Time 3 is out!'), 2000);
+    setImmediate(() => console.log('immediate 1 finished'));
 
-process.nextTick(() => console.log('wtf'));
-
-crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512', () =>{
-    console.log(Date.now() - start, 'password encrypted');
-});
-crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512', () =>{
-    console.log(Date.now() - start, 'password encrypted');
-});
-crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512', () =>{
-    console.log(Date.now() - start, 'password encrypted');
-});
-crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512', () =>{
-    console.log(Date.now() - start, 'password encrypted');
-});
+    process.nextTick(() => console.log('wtf'));
+    
+    crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512', () =>{
+        console.log(Date.now() - start, 'password encrypted');
+    });
+    crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512', () =>{
+        console.log(Date.now() - start, 'password encrypted');
+    });
+    crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512', () =>{
+        console.log(Date.now() - start, 'password encrypted');
+    });
+    crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512', () =>{
+        console.log(Date.now() - start, 'password encrypted');
+    });
 });
 
 console.log('hello, top level code here!');
