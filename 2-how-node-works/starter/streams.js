2b399faeab4b5edcb38022e8b590bd709bd03977
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-10 11:12:11 +0000

    solution 2

--- 2-how-node-works/starter/streams.js
+++ 2-how-node-works/starter/streams.js
@@ -4,15 +4,31 @@ const fs = require('fs');
 // criar a variável server, criamos ela junto com o
 // require
 const server = require('http').createServer();
-// reding a huge file to send back to the user
-// =========== BASIC WAY ===============
-server.on('request', (req, res) => {
-    fs.readFile('test-file.txt', (err, data) => {
-        if (err) console.log('Oops! Something went wrong! 🤔');
-        res.end(data);
-    });
-});
-
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening on port 8000...');
 });
+// reding a huge file to send back to the user
+
+server.on('request', (req, res) => {
+// =========== Solution 1 - WITHOUT STREAM ===============
+// works jsut fine, but it loads the whole file into
+// memory, 'cuz only after its ready, it can then send
+
+    // fs.readFile('test-file.txt', (err, data) => {
+    //     if (err) console.log('Oops! Something went wrong! 🤔');
+    //     res.end(data);
+    // });
+
+// =========== Solution 2 - USING STREAM =================
+// We don't need to save the data into a variable
+// creating a readable stream, and when we recieve the
+// chunks of data, we'll send back to the client as a 
+// writable stream
+
+    const readable = fs.createReadStream('test-file.txt');
+    readable.on('data', chunk => {
+        res.write(chunk)
+    })
+
+});
+
