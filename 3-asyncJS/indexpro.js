16ec8b1f47e3ace40ce577b8224badfd2ec5aae9
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-10 16:55:09 +0000

    atualiza URL da imagem do cachorro e melhora comentários no código para maior clareza

--- 3-asyncJS/indexpro.js
+++ 3-asyncJS/indexpro.js
@@ -4,6 +4,15 @@ const fs = require("fs");
 const superagent = require("superagent");
 
 // creating a promise to read a file
+// "file" is the parameter that will be passed to the function
+// example syntax: readFilePro("filename")
+// when the function is called, it returns a new Promise
+// inside the promise, we use fs.readFile to read the file
+// fs.readFile takes two parameters: the file name and a callback function
+// the callback function takes two parameters: err and data
+// err is the error object, and data is the file content
+// if there is an error reading the file, we reject the promise with an error message
+// if the file is read successfully, we resolve the promise with the file content
 const readFilePro = (file) => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
@@ -14,6 +23,14 @@ const readFilePro = (file) => {
 };
 
 // creating a promise to write a file
+// "file" is the file name, and "data" is the content to be written to the file
+// example syntax: writeFilePro("filename", data)
+// when the function is called, it returns a new Promise
+// inside the promise, we use fs.writeFile to write the file
+// fs.writeFile takes three parameters: the file name, the data to be written, and a callback function
+// the callback function takes one parameter: err
+// if there is an error writing the file, we reject the promise with an error message
+// if the file is written successfully, we resolve the promise with a success message
 const writeFilePro = (file, data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(file, data, (err) => {
