2d90bddc8f8576b790c990a38158cbc8a4279e91
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-10 10:55:40 +0000

    how streams work

---
+++ 2-how-node-works/starter/streams.js
@@ -0,0 +1,18 @@
+//           HOW STREAMS WORK - TOP LVL CODE HERE
+const fs = require('fs');
+// Ao invés de criar uma variavel http para depois
+// criar a variável server, criamos ela junto com o
+// require
+const server = require('http').createServer;
+// reding a huge file to send back to the user
+// =========== BASIC WAY ===============
+server.on('request', (req, res) => {
+    fs.readFile('test-file.txt', (err, data) => {
+        if (err) console.log('Oops! Something went wrong! 🤔');
+        res.end(data);
+    });
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening on port 8000...');
+});
