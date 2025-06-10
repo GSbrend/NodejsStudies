6f4fab692fb236aaf6ecf21467caa309b50e6f18
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-10 14:55:12 +0000

    atualiza raça do cachorro para labrador e renomeia arquivo de imagem para dog-img.txt

--- 3-asyncJS/index.js
+++ 3-asyncJS/index.js
@@ -1,14 +1,15 @@
-const fs = require("fs");
-const superagent = require("superagent");
+const fs = require('fs');
+const spa = require('superagent');
+
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Raça: ${data}`);
 
-  superagent
+  spa
     .get(`https://dog.ceo/api/breed/${data}/image/random`)
     .then((res) => {
       console.log(res.body.message);
 
-      fs.writeFile('dog-image.txt', res.body.message, (err) => {
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
         if (err) return console.log(err.message);
         console.log("Imagem salva!");
       });
