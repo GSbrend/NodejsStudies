5d205ee5ffa34159937d8b7de758f61773b1d939
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-10 12:42:44 +0000

    corrige comentários de exportação para consistência e clareza

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -7,11 +7,11 @@ const C = require("./testmodule1");
 const calc1 = new C();
 console.log(calc1.add(5, 12));
 
-// using export
-// saving into a variable
+// // using export
+// // saving into a variable
 // const calc2 = require("./testmodule2");
 // console.log(calc2.multiply(5, 12));
-// using destructuring
+// // using destructuring
 const { add, devide, multiply } = require("./testmodule2");
 console.log(add(5, 12));
 console.log(devide(5, 12));
