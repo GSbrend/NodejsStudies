8eecb7ad27032ee3fda98e83c2314f62669f502a
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-10 16:16:19 +0000

    atualiza URL da imagem do cachorro e implementa lógica para buscar e salvar imagem aleatória da API

--- 3-asyncJS/index.js
+++ 3-asyncJS/index.js
@@ -6,4 +6,23 @@ const superagent = require("superagent");
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Raça: ${data}`);
+  // using superagent to fetch a random dog image from the API
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      if (err) {
+        // if there is an error, log the error message
+        return console.error("Error fetching dog image:", err.message);
+      }
+      // writing the random dog image URL
+      console.log(res.body.message);
+      fs.writeFile("dog-img.txt", res.body.message, (err) => {
+        if (err) {
+          // if there is an error writing the file, log the error message
+          return console.log("error writing file:", err.message);
+        }
+        // if the file is written successfully, log a success message
+        console.log("Random dog image saved to file");
+      });
+    });
 });
