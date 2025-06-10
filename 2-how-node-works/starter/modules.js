ccbb438ca848727e79deef869915f7ced4ad417c
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-10 12:34:27 +0000

    adiciona funções de operação matemática em testmodule2.js e organiza comentários em modules.js

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -2,7 +2,9 @@
 // console.log(require('module').wrapper);
 
 //  export/import data from one module into the other
+// using module.exports
 const C = require('./testmodule1')
-
 const calc1 = new C();
 console.log(calc1.add(5,12));
+
+// using export
