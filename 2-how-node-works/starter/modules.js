972f744c336ca22ea1c112c8972d84af1f5d0b1c
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-10 12:39:24 +0000

    formata código em modules.js para consistência e legibilidade

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -3,8 +3,16 @@
 
 //  export/import data from one module into the other
 // using module.exports
-const C = require('./testmodule1')
+const C = require("./testmodule1");
 const calc1 = new C();
-console.log(calc1.add(5,12));
+console.log(calc1.add(5, 12));
 
 // using export
+// saving into a variable
+const calc2 = require("./testmodule2");
+console.log(calc2.multiply(5, 12));
+// using destructuring
+const { add, devide, multiply } = require("./testmodule2");
+console.log(add(5, 12));
+console.log(devide(5, 12));
+console.log(multiply(5, 12));
