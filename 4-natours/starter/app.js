dfa52ff55e69b90a04eae39bb62046a9956b72df
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-11 16:53:53 +0000

    adiciona configuração básica do servidor Express no app.js

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,1 +1,15 @@
 import express from 'express';
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.status(200)
+  .json({ message: 'Hello World!', app: 'natours_b' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint, but it does not do anything yet.');
+}
+);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
