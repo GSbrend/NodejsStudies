6ef4d4f0dc69dfef055399862c72e6c7f96a148b
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-11 19:44:46 +0000

    altera a importação de módulos para o formato CommonJS e remove a propriedade "type" do package.json

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,27 +1,16 @@
-import express from 'express';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
-const __filepath = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filepath);
-const overviewTemplate = fs.readFileSync(
-  path.join(__dirname, '/public/overview.html'),
-  'utf-8'
-);
-const toursData = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '/dev-data/data/tours-simple.json'))
-);
+const toursData = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-app.use(express.json()); // Middleware para analisar JSON no corpo das requisições
+app.use(express.json());
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-app.get('/', (req, res) => {
-  res.status(200).send(overviewTemplate);
+  console.log(`Servidor está rodando na porta ${port}`);
 });
 
 app.get('/api/v1/tours', (req, res) => {
@@ -30,27 +19,25 @@ app.get('/api/v1/tours', (req, res) => {
     results: toursData.length, // É uma boa prática incluir o número de resultados
     data: {
       tours: toursData,
-    }
+    },
   });
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  //console.log(req.body);
+  // // phase 1
+  // console.log(req.body);
+  // res.send('done');
+
+  // phase 2
   const newId = toursData[toursData.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
-  // Adiciona o novo tour ao array de tours
   toursData.push(newTour);
-
-  fs.writeFile(path.join(__dirname, '/dev-data/data/tours-simple.json'),
-    JSON.stringify(toursData),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
-    }
-  );
-  res.end('done!');
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(toursData), (err) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  });
 });
