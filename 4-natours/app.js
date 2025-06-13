43c86b50e1e98a0c38ca94c234bc8bc8bde9a001
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-13 15:23:42 +0000

    fix: corrigir formatação e adicionar ponto e vírgula em app.js e server.js

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,16 +2,17 @@
 
 const express = require('express');
 const morgan = require('morgan');
+
 const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // 1) GLOBAL MIDDLEWARES
-if (process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev')); // Log requests to the console
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev')); // Log requests to the console
 }
 app.use(express.json()); // Parse JSON bodies (as sent)
-app.use(express.static(`${__dirname}/public`)) // used to manipulate URL of static files in the folder
+app.use(express.static(`${__dirname}/public`)); // used to manipulate URL of static files in the folder
 
 // 2) ROUTES
 app.use('/api/v1/tours', tourRouter);
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,12 +1,11 @@
 // isn't express? so be my guest!
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
-
 
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Servidor está rodando na porta ${port}...`);
 });
-
