fb3d9b0a896d0b837e6d9e182eddfddedb6169c7
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-13 11:54:01 +0000

    fix: corrigir a falta de ponto e vírgula na configuração do middleware estático

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -7,7 +7,7 @@ const userRouter = require('./routes/userRoutes');
 // 1) GLOBAL MIDDLEWARES
 app.use(morgan('dev')); // Log requests to the console
 app.use(express.json()); // Parse JSON bodies (as sent)
-
+app.use(express.static(`${__dirname}/public`))
 // 2) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
