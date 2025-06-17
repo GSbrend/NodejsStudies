1996036f151bac5f5fcc7b30af7bc2ef5e24082c
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-17 15:10:44 +0000

    .

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,9 +1,19 @@
 // isn't express? so be my guest!
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+mongoose.connect(DB, {
+  useNewUrlParses: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(con = {
+  console.log(con.connections);
+  console.log
+})
 
 const port = process.env.PORT;
 app.listen(port, () => {
