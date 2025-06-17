5a341172634564f70c0ec0bff8e9deccdfd81eda
GSbrend <99551425+GSbrend@users.noreply.github.com>
2025-06-17 15:41:50 +0000

    fix: ajustar opções de conexão do mongoose

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -11,14 +11,12 @@ const DB = process.env.DATABASE.replace("<password>", process.env.DB_PASSWORD);
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
   })
-  .then((con) => {
-    console.log(con.connections);
-    console.log("DB connection successfull");
-  });
-
+  .then((con) => console.log("DB connection successfull"));
+  
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Servidor está rodando na porta ${port}...`);
